374b640dff1015acc32fa7e262bb62d5ddf30f4b
Xiaofei Li <xiaofeili.me@gmail.com>
2022-04-30 13:44:25 +0200

    add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatTabsModule} from '@angular/material/tabs';
+import { ListeningComponent } from './components/listening/listening.component';
+import { SpeakingComponent } from './components/speaking/speaking.component';
+import { WritingComponent } from './components/writing/writing.component';
+import { VocabularyComponent } from './components/vocabulary/vocabulary.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeningComponent,
+    SpeakingComponent,
+    WritingComponent,
+    VocabularyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatTabsModule,
   ],
+  // exports: [
+  //   MatSliderModule
+  // ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/listening/listening.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListeningComponent } from './listening.component';
+
+describe('ListeningComponent', () => {
+  let component: ListeningComponent;
+  let fixture: ComponentFixture<ListeningComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListeningComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListeningComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
